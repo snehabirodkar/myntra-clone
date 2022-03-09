6d21ba35030d3f61c9d0289e75ab1b36171006b8
SnehaBirodkar <snehabirodkar31@gmail.com>
2022-03-09 22:51:54 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React from 'react'
-import Cards from './components/Cards';
-import Pdata from "./components/Pdata";
+import React from "react";
+import "./App.css";
+import Cards from "./components/Product/Cards";
+import Pdata from "./components/Product/Pdata";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Filter from "./components/Filter/Filter.jsx";
+import Sort from "./components/Sort/Sort.jsx";
 
 const App = () => {
-  return(
-      <>
-	  <Navbar />
-	  <div className="flexed">
-	  {
-          Pdata.map((val, index) => {
-            return(
-              <Cards
+  return (
+    <>
+      <Navbar />
+	  <Sort />
+      <div className="outer-wrapper">
+        <div className="filter-left-content">
+          <Filter />
+        </div>
+        <div className="container left-content-border">
+          <div className="flexed">
+            {Pdata.map((val, index) => {
+              return (
+                <Cards
                   key={index}
-                  imgsrc = {val.imgscr}
-                  pname = {val.pname}
-                  product = {val.product}
-                  price = {val.price}
-              />
-            )
-          })
-        }
-	  </div>
-
-      </>
-  )
-}
+                  imgsrc={val.imgscr}
+                  pname={val.pname}
+                  product={val.product}
+                  price={val.price}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,61 @@
 import React from "react";
 import Images from "../Images";
 import './Navbar.css';
+import myntraLogo from "../myntra.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBagShopping, faSearch, faUser, faHeart} from '@fortawesome/free-solid-svg-icons'
 
-const menu = ["Mens","Womens","Kids","Home & Living", "Beauty"];
+const menu = ["Mens","Womens","Kids","Home & Living", "Offer"];
 function Navbar(){
 	return (
-		<div className="">
+		<nav id="navbar">
+			<div className="container navigation">
+			<div className="content-wrapper">
+			<div className="flexed">
 			{/* Logo Section */}
 			<div className="">
-			<img src="https://wallpapercave.com/wp/wp9101967.jpg" alt="myntra logo" className="" width="70" height="50"/>
+			<img src={myntraLogo} alt="myntra logo" className="" width="70" height="50"/>
 			</div>
 			{/* Menu section */}
-			<div className="">
+			<div className="flexed">
 				{
 					menu.map(item=>(
-						<div className="" key={item}>
+						<div className="inner-content" key={item}>
 							<p>{item}</p>
 						</div>
 					))
 				}
+
 			</div>
 			{/* Search Section */}
-			<div className="">
-				<input type="search" placeholder="Search for Products..." />
+			<div className="border">
+			<FontAwesomeIcon icon={faSearch} className="font-color"/>
+				<input className = "padding	" type="search" placeholder="Search for Products, brands and more..." />
 			</div>
 			{/* User profile */}
-			<div className=""></div>
+			<div className="flexed">
+				{/* Profile */}
+				<div className="p-r-10">
+				<FontAwesomeIcon icon={faUser} className="font-color" />
+				<p>Profile</p>
+				</div>
+
+				{/* Whishlist */}
+				<div className="p-r-10">
+				<FontAwesomeIcon icon={faHeart} className="font-color" />
+				<p>Wishlist</p>
+				</div>
+
+				{/* Cart */}
+				<div className="p-r-10">
+				<FontAwesomeIcon icon={faBagShopping} className="font-color" />
+				<p>Cart</p>
+				</div>
+			</div>
 		</div>
+			</div>
+		</div>
+		</nav>
 	)
  }
  export default Navbar;
